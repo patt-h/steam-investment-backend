ca3935d972c71177051dfa0873ee17f0bd2f483b
patt-h <patryk.huchla@wp.pl>
2024-08-22 16:57:13 +0200

    Initial commit

---
+++ src/main/java/com/example/steam_investment_backend/SteamInvestmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.steam_investment_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamInvestmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamInvestmentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/steam_investment_backend/SteamInvestmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.steam_investment_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamInvestmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
