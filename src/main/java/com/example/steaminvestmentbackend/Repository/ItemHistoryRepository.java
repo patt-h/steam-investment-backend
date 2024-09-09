27f479e0c5fa7ddaf48b97a0f78c6973d99de015
patt-h <patryk.huchla@wp.pl>
2024-09-09 17:26:50 +0200

    Fixed fetching current item price (today's price was not added to list when it already existed in database), changed findByItemIdAndDate in HistoryRepository to Object type instead of a list, updated Steam token

--- src/main/java/com/example/steaminvestmentbackend/Repository/ItemHistoryRepository.java
+++ src/main/java/com/example/steaminvestmentbackend/Repository/ItemHistoryRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> {
 
     List<ItemHistory> findByItemId(Long itemId);
 
     @Query("SELECT i FROM ItemHistory i WHERE i.itemId = :itemId AND i.date = :date")
-    List<ItemHistory> findByItemIdAndDate(@Param("itemId") Long itemId, @Param("date") LocalDate date);
+    ItemHistory findByItemIdAndDate(@Param("itemId") Long itemId, @Param("date") LocalDate date);
 
 }
--- src/main/java/com/example/steaminvestmentbackend/Service/ItemHistoryService.java
+++ src/main/java/com/example/steaminvestmentbackend/Service/ItemHistoryService.java
@@ -7,11 +7,12 @@ import com.example.steaminvestmentbackend.Entity.ItemList;
 import com.example.steaminvestmentbackend.Exceptions.AppException;
 import com.example.steaminvestmentbackend.Repository.ItemHistoryRepository;
 import com.example.steaminvestmentbackend.Repository.ItemListRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -37,9 +38,14 @@ public class ItemHistoryService {
         if (history.isEmpty()) {
             Optional<ItemList> item = itemListRepository.findById(id);
             if (item.isPresent()) {
-                System.out.println("Fetching data from Steam");
-                List<ItemHistory> fetchHistory = fetchAndParseData(item.get().getMarketHashName(), item.get().getId());
-                itemHistoryRepository.saveAll(fetchHistory);
+                try {
+                    System.out.println("Fetching data from Steam");
+                    List<ItemHistory> fetchHistory = fetchAndParseData(item.get().getMarketHashName(), item.get().getId());
+                    itemHistoryRepository.saveAll(fetchHistory);
+                } catch (AppException e) {
+                    System.out.println(e.getStatus());
+                    throw new AppException("Could not fetch item history due to API limits", HttpStatus.SERVICE_UNAVAILABLE);
+                }
             } else {
                 throw new AppException("Item with provided id doesn't exist", HttpStatus.NOT_FOUND);
             }
@@ -52,17 +58,20 @@ public class ItemHistoryService {
         List<ItemHistory> prices = new ArrayList<>();
 
         for (Long itemId : id) {
-            List<ItemHistory> history = itemHistoryRepository.findByItemIdAndDate(itemId, LocalDate.now());
-            if (history.isEmpty()) {
+            ItemHistory history = itemHistoryRepository.findByItemIdAndDate(itemId, LocalDate.now());
+            if (history == null) {
                 Optional<ItemList> item = itemListRepository.findById(itemId);
                 if (item.isPresent()) {
-                    System.out.println("Fetching data from Steam");
+                    System.out.println("Fetching current price data from Steam");
                     ItemHistory fetchCurrent = fetchCurrentData(item.get().getMarketHashName(), item.get().getId());
                     itemHistoryRepository.save(fetchCurrent);
                     prices.add(fetchCurrent);
                 } else {
                     throw new AppException("Item with provided id doesn't exist", HttpStatus.NOT_FOUND);
                 }
+            }
+            else {
+                prices.add(history);
             }
         }
 
@@ -73,50 +82,52 @@ public class ItemHistoryService {
         String url = "https://steamcommunity.com/market/pricehistory/?appid=730&market_hash_name=" + marketHashName;
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders requestHeaders = new HttpHeaders();
-        requestHeaders.add("Cookie", "steamLoginSecure=76561198407317357%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0" +
-                ".eyAiaXNzIjogInI6MEY0M18yNEVFQkQ4RF82NDFBRiIsICJzdWIiOiAiNzY1NjExOTg0MDczMTczNTciLCAiYXVkI" +
-                "jogWyAid2ViOmNvbW11bml0eSIgXSwgImV4cCI6IDE3MjQ5NDMzMzUsICJuYmYiOiAxNzE2MjE1MjY4LCAiaWF0I" +
-                "jogMTcyNDg1NTI2OCwgImp0aSI6ICIxMDU1XzI0RjdGNzI4XzI4ODBGIiwgIm9hdCI6IDE3MjQzNDMyOTcsICJydF9leHAiOiAxNzQyNzc5ODU1LCAicGVyI" +
-                "jogMCwgImlwX3N1YmplY3QiOiAiODkuNjQuMTUuNDIiLCAiaXBfY29uZmlybWVyIjogIjg5LjY0LjE1LjQyIiB9" +
-                ".TRhvp7YIoiz6Og2hDSCcDiu5CSXSnY2DzZ8U7iIheKovOHV-9YM8xIP31yXZ28qrjTh6vzx9VAbYro3kMRg8BQ");
-        HttpEntity requestEntity = new HttpEntity(null, requestHeaders);
-
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                requestEntity,
-                String.class
-        );
-
-        String jsonData = responseEntity.getBody();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        SteamHistoryDTO priceResponse = objectMapper.readValue(jsonData, SteamHistoryDTO.class);
+        requestHeaders.add("Cookie", "steamLoginSecure=76561198407317357%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0." +
+                "eyAiaXNzIjogInI6MEY0M18yNEVFQkQ4RF82NDFBRiIsICJzdWIiOiAiNzY1NjExOTg0MDczMTczNTciLCAiYXVkI" +
+                "jogWyAid2ViOmNvbW11bml0eSIgXSwgImV4cCI6IDE3MjU5NjY3NTQsICJuYmYiOiAxNzE3MjM4OTE2LCAiaWF0I" +
+                "jogMTcyNTg3ODkxNiwgImp0aSI6ICIxMDY4XzI1MDEzMjg3XzcyNzA0IiwgIm9hdCI6IDE3MjQzNDMyOTcsICJydF9leHAiOiAxNzQyNzc5ODU1LCAicGVyI" +
+                "jogMCwgImlwX3N1YmplY3QiOiAiODkuNjQuMTUuNDIiLCAiaXBfY29uZmlybWVyIjogIjg5LjY0LjE1LjQyIiB9." +
+                "HTb7xrY__OPljGYusYI-d409Z3oSgP2hQq9J76ZSCUizUe8bWTS9RzH2iWF-V-xrNNk40E7XPXWmrUEMjJ4yCw");
+        HttpEntity<?> requestEntity = new HttpEntity<>(null, requestHeaders);
 
         List<ItemHistory> itemHistoryList = new ArrayList<>();
-
         LocalDate today = LocalDate.now();
         LocalDate oneYearAgo = today.minusYears(1);
-
         Set<LocalDate> processedDates = new HashSet<>();
 
-        for (List<String> priceData : priceResponse.getPrices()) {
-            String dateString = priceData.get(0).substring(0, 11); // np. "May 27 2015"
-            LocalDate date = LocalDate.parse(dateString, formatter);
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    requestEntity,
+                    String.class
+            );
 
-            if (date.isAfter(oneYearAgo) && date.isBefore(today)) {
-                if (!processedDates.contains(date)) {
-                    Float price = Float.parseFloat(priceData.get(1));
+            String jsonData = responseEntity.getBody();
 
-                    ItemHistory itemHistory = new ItemHistory();
-                    itemHistory.setDate(date);
-                    itemHistory.setPrice(price);
-                    itemHistory.setItemId(itemId);
+            ObjectMapper objectMapper = new ObjectMapper();
+            SteamHistoryDTO priceResponse = objectMapper.readValue(jsonData, SteamHistoryDTO.class);
 
-                    itemHistoryList.add(itemHistory);
-                    processedDates.add(date);
+            for (List<String> priceData : priceResponse.getPrices()) {
+                String dateString = priceData.get(0).substring(0, 11); // np. "May 27 2015"
+                LocalDate date = LocalDate.parse(dateString, formatter);
+
+                if (date.isAfter(oneYearAgo) && date.isBefore(today)) {
+                    if (!processedDates.contains(date)) {
+                        Float price = Float.parseFloat(priceData.get(1));
+
+                        ItemHistory itemHistory = new ItemHistory();
+                        itemHistory.setDate(date);
+                        itemHistory.setPrice(price);
+                        itemHistory.setItemId(itemId);
+
+                        itemHistoryList.add(itemHistory);
+                        processedDates.add(date);
+                    }
                 }
             }
+        } catch (HttpClientErrorException e) {
+            throw new AppException("Too Many Requests", HttpStatus.TOO_MANY_REQUESTS);
         }
 
         return itemHistoryList;
